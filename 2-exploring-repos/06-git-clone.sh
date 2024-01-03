7db47cea7823795ae00ea6873fd64c00204a5d50
Dusch4593 <brandondusch@gmail.com>
2024-01-03 12:54:37 -0500

    small edit

--- 2-exploring-repos/06-git-clone.sh
+++ 2-exploring-repos/06-git-clone.sh
@@ -2,4 +2,4 @@
 # Git Clone ©
 
 # Make sure to replace "your-github-username" with your actual username
-git clone https://github.com/your-github-username/github-chapter-2-contributions.git
+git clone https://github.com/your-github-username/github-chapter-2-sandbox.git
