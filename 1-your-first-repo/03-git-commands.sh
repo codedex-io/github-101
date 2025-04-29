71c42b4854db76baa31b5782cf5df5a31aa64112
Sonny Li <sonnynomnom@gmail.com>
2025-04-29 11:09:22 -0400

    Update 03-git-commands.sh

--- 1-your-first-repo/03-git-commands.sh
+++ 1-your-first-repo/03-git-commands.sh
@@ -1,5 +1,13 @@
-# Codédex
 # Git Commands 📺
+# Codédex
+
+# Make sure you're changed into your project folder first:
+
+pwd
+ls
+cd recipe-app
+
+# Run this:
 
 git init
 git remote add origin https://github.com/username/repo-name.git
