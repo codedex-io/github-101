6e4854164a5e96089e0ca3d940ed8b6649e4c2eb
Sonny Li <sonnynomnom@gmail.com>
2025-04-29 11:08:32 -0400

    Update 02-repositories.sh

--- 1-your-first-repo/02-repositories.sh
+++ 1-your-first-repo/02-repositories.sh
@@ -1,7 +1,4 @@
-# Codédex
 # Repositories 📂
+# Codédex
 
-# Make sure you're changed into your project folder before running this:
-git init
-git remote add origin https://github.com/username/recipe-app.git
-git branch -M main
+# You don't have to do anything in the terminal here!
