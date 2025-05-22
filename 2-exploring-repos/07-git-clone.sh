74439c0bfbf1f6b92a46411ffc6f1d0d7fcbe6de
Dharmarajsinh Jethva <jethvadharmarajsinh@gmail.com>
2025-05-22 18:41:43 +0530

    Update 07-git-clone.sh

--- 2-exploring-repos/07-git-clone.sh
+++ 2-exploring-repos/07-git-clone.sh
@@ -1,5 +1,6 @@
 # Codédex
 # Git Clone ©
 
-# Make sure to replace "your-github-username" with your actual username
+# Fork the original Codédex repository. Followed by cloning it. 
+# Make sure to replace "your-github-username" with your actual username for cloning the repository.
 git clone https://github.com/your-github-username/github-chapter-2-sandbox.git
